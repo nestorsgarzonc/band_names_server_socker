e8643ef33fa888f617f4efa3fca4f54556dfa2e8
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2021-01-18 10:48:09 -0500

    added token for demo propourses and fix in socket

--- controllers/socket.js
+++ controllers/socket.js
@@ -1,14 +1,14 @@
 const User = require('../models/user')
 
 const connectedUser = async (uid = '') => {
-    const user = await User.findById({ uid })
+    const user = await User.findById(uid)
     user.online = true;
     await user.save();
     return user;
 }
 
 const disconnectedUser = async (uid = '') => {
-    const user = await User.findById({ uid })
+    const user = await User.findById(uid)
     user.online = false;
     await user.save();
     return user;
